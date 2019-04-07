43fe851c9ad3e9b335bd18ca2c9777f1e610c123
hwadii <wadii.hajji@etu.univ-amu.fr>
2019-04-07 22:25:52 +0200

    add feature to upload image and display it in canvas

--- public/index.js
+++ public/index.js
@@ -1,5 +1,20 @@
 const melanoma = document.getElementById('melanoma');
+const resId = document.getElementById('res');
 
-const handleFile = file => {
-  console.log(file);
-};
+(async () => {
+  const res = await fetch('/stuff');
+  const kernelClose = await res.json();
+})();
+
+const handleFile = files => {
+  const img = new Image();
+  img.src = URL.createObjectURL(files[0]);
+  img.onload = () => {
+    const canvas = document.getElementById('img');
+    const ctx = canvas.getContext('2d');
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    ctx.drawImage(img, 0, 0);
+  }
+}
